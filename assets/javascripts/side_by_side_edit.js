ecef6e9a6c4139410b73d72fd701635342397852
Joris Kraak <jkraak@gnresound.com>
2012-10-03 17:44:48 +0200

    Renames side by side mode to enable creation of additional modes

--- assets/javascripts/side_by_side_edit.js
+++ assets/javascripts/side_by_side_edit.js
@@ -4,12 +4,12 @@ jQuery(function($) {
   var preview = $('div#preview');
   var preview_link = $('input[name=commit]').next();
 
-  var disable = function() {
+  var disable_side_by_side_mode = function() {
     editor_parent.add(preview).removeClass('side_by_side');
     $('#content').append(preview);
   };
 
-  var enable = function() {
+  var enable_side_by_side_mode = function() {
     editor_parent.add(preview).addClass('side_by_side');
 
     editor_parent.before(preview);
@@ -28,17 +28,17 @@ jQuery(function($) {
     var target = $(event.data);
 
     target.scrollTop(source_position * (target[0].scrollHeight - target.innerHeight()));
-  }
+  };
 
-  var toggle_mode = function() {
+  var toggle_side_by_side_mode = function(event) {
     if(this.checked) {
-      enable();
+      enable_side_by_side_mode();
     } else {
-      disable();
+      disable_side_by_side_mode();
     }
-  }
+  };
 
   preview_link.after($('<label for="side_by_side_mode">Show editor and preview side-by-side</label>'))
               .after($('<input id="side_by_side_mode" type="checkbox" />'));
-  $('#side_by_side_mode').on('click', toggle_mode);
+  $('#side_by_side_mode').click(toggle_side_by_side_mode);
 });
