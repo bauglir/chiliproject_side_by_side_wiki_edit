485726cc2291e8357f479cd42e39eda553b57054
Joris Kraak <jkraak@gnresound.com>
2012-12-06 13:41:09 +0100

    The plugin now also shows when creating new wiki pages. Fixes #1
    The plugin was previously only inserted on the edit action of the
    WikiController. It is now also inserted on the show action, which
    ChiliProject uses to create new wiki pages and to show them. This does
    mean that for now that plugin also gets loaded when the wiki page editor
    is in fact not available

--- lib/chiliproject_side_by_side_wiki_edit/hooks/view_wiki_edit_hook.rb
+++ lib/chiliproject_side_by_side_wiki_edit/hooks/view_wiki_edit_hook.rb
@@ -2,13 +2,24 @@ module ChiliprojectSideBySideWikiEdit
   module Hooks
     class ViewWikiEditHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context = {})
-        if context[:controller] && context[:controller].is_a?(WikiController) && (context[:controller].action_name == 'edit')
+        if wiki_controller?(context) && extendable_action?(context)
           asset_path = '/plugin_assets/chiliproject_side_by_side_wiki_edit'
           [
             javascript_include_tag("#{asset_path}/javascripts/side_by_side_edit.js"),
             stylesheet_link_tag("#{asset_path}/stylesheets/side_by_side_edit.css")
           ]
         end
+      end
+
+      def extendable_action?(context)
+        # For some reason ChiliProject uses the show action instead of the new
+        # action to create new wiki pages. This does mean that the javascript
+        # and stylesheets get loaded on regular wiki pages as well
+        context[:controller] && ['show', 'edit'].include?(context[:controller].action_name)
+      end
+
+      def wiki_controller?(context)
+        context[:controller] && context[:controller].is_a?(WikiController)
       end
     end
   end
