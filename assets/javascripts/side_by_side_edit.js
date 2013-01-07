8fd402b185bdc36e442e07e9e2ad62cc32354930
Joris Kraak <jkraak@gnresound.com>
2013-01-07 15:34:59 +0100

    Resizing the editor area resizes the preview
    On some browsers it is possible to resize the editor textarea. Such
    changes should be reflected in the preview window.
    
    This behavior is now implemented by listening to and acting on the
    mouseup event on the textarea. The current implementation is a
    workaround workaround since the resize event does not work on individual
    HTML elements

--- assets/javascripts/side_by_side_edit.js
+++ assets/javascripts/side_by_side_edit.js
@@ -7,6 +7,8 @@ jQuery(function($) {
     editor.add(preview).removeClass('side_by_side');
 
     editor.off('scroll');
+    editor.off('mouseup');
+
     $('#content').append(preview);
   };
 
@@ -14,15 +16,29 @@ jQuery(function($) {
     editor.add(preview).addClass('side_by_side');
 
     editor.before(preview);
-    preview.height(editor.height() + 5);
+    resize_preview_to_editor_height();
 
     editor.scroll(preview, scroll_preview);
+    editor.mouseup(resize_preview_if_required);
     editor.scroll();
 
     if($('fieldset.preview').length === 0) {
       preview_link.click();
     }
   };
+
+  var resize_preview_if_required = function() {
+    var newEditorHeight = editor.outerHeight();
+
+    if(newEditorHeight != editor.data('height')) {
+      editor.data('height', newEditorHeight);
+      resize_preview_to_editor_height();
+    }
+  }
+
+  var resize_preview_to_editor_height = function() {
+    preview.height(editor.height() + 6);
+  }
 
   var scroll_preview = function(event) {
     var source_position = this.scrollTop / (this.scrollHeight - $(this).innerHeight());
@@ -42,4 +58,6 @@ jQuery(function($) {
   preview_link.after($('<label for="side_by_side_mode">Show editor and preview side-by-side</label>'))
               .after($('<input id="side_by_side_mode" type="checkbox" />'));
   $('#side_by_side_mode').click(toggle_side_by_side_mode);
+
+  editor.data('height', editor.outerHeight());
 });
